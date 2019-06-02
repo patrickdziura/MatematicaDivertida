45be5d50709f96b1fe127bb4ad1fe272855b1a46
patrickdziura@gmail.com <Naruto_1996>
2019-06-01 22:21:15 -0300

    Verificação do resultado e alertDialog feitos

--- app/src/main/java/com/example/matematicadivertida/AritmeticaBasica.java
+++ app/src/main/java/com/example/matematicadivertida/AritmeticaBasica.java
@@ -1,18 +1,27 @@
 package com.example.matematicadivertida;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.Random;
 
 public class AritmeticaBasica extends AppCompatActivity {
 
+    int resultado;
+    int acertos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aritmetica_basica);
-        geraConta();
+        acertos = 0;
+        int i;
+        for(i=0; i< 5; i++) {
+            geraConta();
+        }
     }
 
     private void geraConta() {
@@ -27,8 +36,32 @@ public class AritmeticaBasica extends AppCompatActivity {
         op2.setText(Integer.toString(operando2));
         if (opaux == 0) {
             operador.setText("+");
+            resultado = operando1 + operando2;
         } else {
             operador.setText("-");
+            resultado = operando1 - operando2;
+        }
+        System.out.println(resultado);
+    }
+
+    public void verificaResultado (View view) {
+        EditText result = (EditText) findViewById(R.id.editResultado);
+        int intresult = Integer.parseInt(result.getText().toString());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alert;
+        builder.setPositiveButton("OK",null);
+        if (intresult == resultado) {
+            builder.setTitle("Resultado Correto");
+            builder.setMessage("O resultado está correto!");
+            alert = builder.create();
+            alert.show();
+            acertos++;
+            //new AlertDialog.Builder(this).setTitle("Resultado Correto").setMessage("O resultado está correto!");
+        } else {
+            builder.setTitle("Resultado Errado");
+            builder.setMessage("O resultado correto é: " + String.valueOf(resultado));
+            alert = builder.create();
+            alert.show();
         }
     }
 }
