e6402edba219cb756ec3bdf31e014b40c01b004a
patrickdziura@gmail.com <Naruto_1996>
2019-06-02 00:12:10 -0300

    Correção alertDialog final

--- app/src/main/java/com/example/matematicadivertida/AritmeticaBasica.java
+++ app/src/main/java/com/example/matematicadivertida/AritmeticaBasica.java
@@ -1,10 +1,12 @@
 package com.example.matematicadivertida;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -12,22 +14,27 @@ public class AritmeticaBasica extends AppCompatActivity {
 
     int resultado;
     int acertos;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aritmetica_basica);
         acertos = 0;
-        int i;
-        for(i=0; i< 5; i++) {
-            geraConta();
-        }
+        count = 1;
+        geraConta();
     }
 
+
+
     private void geraConta() {
+        TextView titulo = (TextView) findViewById(R.id.txtTitulo);
         EditText op1 = (EditText) findViewById(R.id.editOp1);
         EditText op2 = (EditText) findViewById(R.id.editOp2);
         EditText operador = (EditText) findViewById(R.id.editOperador);
+        EditText aux = (EditText) findViewById(R.id.editResultado);
+        aux.setText("");
+        titulo.setText("Pergunta " + String.valueOf(count) + " de 5: ");
         Random r = new Random();
         int operando1 = r.nextInt(10);
         int operando2 = r.nextInt(10);
@@ -49,7 +56,17 @@ public class AritmeticaBasica extends AppCompatActivity {
         int intresult = Integer.parseInt(result.getText().toString());
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         AlertDialog alert;
-        builder.setPositiveButton("OK",null);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (count >= 5) {
+                    criaDialogFinal();
+                } else {
+                    count++;
+                    geraConta();
+                }
+            }
+        });
         if (intresult == resultado) {
             builder.setTitle("Resultado Correto");
             builder.setMessage("O resultado está correto!");
@@ -63,5 +80,19 @@ public class AritmeticaBasica extends AppCompatActivity {
             alert = builder.create();
             alert.show();
         }
+    }
+
+    private void criaDialogFinal() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Fim!");
+        builder.setMessage("Você acertou " + String.valueOf(acertos) + " de 5.");
+        builder.setPositiveButton("Voltar ao menu", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
