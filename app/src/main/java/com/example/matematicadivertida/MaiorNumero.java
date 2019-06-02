a3072c25bf7e4f9d833b79b2719dd82375620753
patrickdziura@gmail.com <Naruto_1996>
2019-06-02 12:42:24 -0300

    Funçao para gerar problema feita

--- app/src/main/java/com/example/matematicadivertida/MaiorNumero.java
+++ app/src/main/java/com/example/matematicadivertida/MaiorNumero.java
@@ -2,12 +2,76 @@ package com.example.matematicadivertida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MaiorNumero extends AppCompatActivity {
+
+    int acertos;
+    int count;
+    int maiornumero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maior_numero);
+        count = 1;
+        acertos = 0;
+        geraProblema();
+    }
+
+    private void geraProblema() {
+        int m1 = 0, m2 = 0, m3 = 0;
+        EditText num1 = (EditText) findViewById(R.id.editNum1);
+        EditText num2 = (EditText) findViewById(R.id.editNum2);
+        EditText num3 = (EditText) findViewById(R.id.editNum3);
+        EditText maiornum = (EditText) findViewById(R.id.editResultado);
+        TextView titulo = (TextView) findViewById(R.id.txtTitulo);
+        titulo.setText("Pergunta " + String.valueOf(count) + " de 5: ");
+        maiornum.setText("");
+        Random r = new Random();
+        int n1 = r.nextInt(10);
+        int n2 = r.nextInt(10);
+        int n3 = r.nextInt(10);
+        num1.setText(Integer.toString(n1));
+        num2.setText(Integer.toString(n2));
+        num3.setText(Integer.toString(n3));
+        if (n1 >= n2 && n1 >= n3) {
+            m1 = n1;
+            if (n2 >= n3) {
+                m2 = n2;
+                m3 = n3;
+            } else {
+                m2 = n3;
+                m3 = n2;
+            }
+        } else if (n2 >= n1 && n2 >= n3) {
+            m1 = n2;
+            if (n1 >= n3) {
+                m2 = n1;
+                m3 = n3;
+            } else {
+                m2 = n3;
+                m3 = n1;
+            }
+        } else if (n3 >= n1 && n3 >= n2) {
+            m1 = n3;
+            if (n1 >= n2) {
+                m2 = n1;
+                m3 = n2;
+            } else {
+                m2 = n2;
+                m3 = n1;
+            }
+        }
+        maiornumero = (100 * m1) + (10 * m2) + m3;
+        System.out.println(maiornumero);
+    }
+
+    public void verificaMaior(View view) {
+
     }
 }
