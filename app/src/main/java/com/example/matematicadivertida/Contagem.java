0e3358d3e0ccb8b242429b3e280a63123cf0d33a
Luiz Dominico <dominico.luiz@gmail.com>
2019-06-12 01:38:07 -0300

    contagem game are done, with small mistakes and bugs probably

--- app/src/main/java/com/example/matematicadivertida/Contagem.java
+++ app/src/main/java/com/example/matematicadivertida/Contagem.java
@@ -1,5 +1,7 @@
 package com.example.matematicadivertida;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Contagem extends AppCompatActivity {
@@ -14,6 +17,11 @@ public class Contagem extends AppCompatActivity {
     TextView titleTextView;
     ImageView randomImageView;
     Button firstButton,secondButton,thirdButton;
+    int rightanswer;
+    int success;
+    int count;
+    ArrayList<Integer> doneimages = new ArrayList<Integer>();
+    int pos = 0;
 
     Integer[] images = {
             R.drawable.one_image,
@@ -39,21 +47,31 @@ public class Contagem extends AppCompatActivity {
         secondButton = findViewById(R.id.secondButton);
         thirdButton = findViewById(R.id.thirdButton);
 
-        generateAll(new View(this));
+        rightanswer = generateAll();
 
     }
 
-    public void generateAll(View view){
+    public int generateAll(){
 
         int number = generateRandomImage(new View(this));
         number++;
         generateNumbers(new View(this),number);
+        return number;
+
     }
 
     public int generateRandomImage(View view) {
 
         Random random = new Random();
         int number = random.nextInt(10);
+
+        while (doneimages.contains(number)) {
+            number = random.nextInt(10);
+        }
+
+        doneimages.add(pos,number);;
+        pos++;
+
         randomImageView.setImageResource(images[number]);
 
         return number;
@@ -66,7 +84,33 @@ public class Contagem extends AppCompatActivity {
         int buttonumber = random.nextInt(3);
         int[] buttonumberarray = {0,0,0};
 
-        buttonumberarray[buttonumber] = number;
+        buttonumberarray[buttonumber] = number; //número correto
+
+        int anotherbuttonnumber = random.nextInt(10);
+
+        while (anotherbuttonnumber == buttonumberarray[buttonumber]) {
+            anotherbuttonnumber = random.nextInt(10);
+        }
+
+        int position = random.nextInt(3);
+        while(position == buttonumber){
+            position = random.nextInt(3);
+        }
+
+        buttonumberarray[position] = anotherbuttonnumber;
+
+        anotherbuttonnumber = random.nextInt(10);
+
+        while (anotherbuttonnumber == buttonumberarray[buttonumber]) {
+            anotherbuttonnumber = random.nextInt(10);
+        }
+
+        position = random.nextInt(3);
+        while(position == buttonumber){
+            position = random.nextInt(3);
+        }
+
+        buttonumberarray[position] = anotherbuttonnumber;
 
         firstButton.setText(Integer.toString(buttonumberarray[0]));
         secondButton.setText(Integer.toString(buttonumberarray[1]));
@@ -74,8 +118,135 @@ public class Contagem extends AppCompatActivity {
 
     }
 
-    public boolean checkAnswer() {
-
-        return false;
+    public boolean checkAnswerFirst() {
+        int answer = Integer.parseInt(firstButton.getText().toString());
+        if(answer == rightanswer){
+            return true;
+        }else{
+            return false;
+        }
     }
+
+    public boolean checkAnswerSecond() {
+        int answer = Integer.parseInt(secondButton.getText().toString());
+        if(answer == rightanswer){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean checkAnswerThird() {
+        int answer = Integer.parseInt(thirdButton.getText().toString());
+        if(answer == rightanswer){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void firstClick(View view){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alert;
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (count >= 4) {
+                    criaDialogFinal();
+                } else {
+                    count++;
+                    rightanswer = generateAll();
+                }
+            }
+        });
+        if (checkAnswerFirst()) {
+            builder.setTitle("Resultado Correto");
+            builder.setMessage("O resultado está correto!");
+            alert = builder.create();
+            alert.show();
+            success++;
+            //new AlertDialog.Builder(this).setTitle("Resultado Correto").setMessage("O resultado está correto!");
+        } else {
+            builder.setTitle("Resultado Errado");
+            builder.setMessage("O resultado correto é: " + String.valueOf(rightanswer));
+            alert = builder.create();
+            alert.show();
+        }
+
+    }
+
+    public void secondClick(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alert;
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (count >= 4) {
+                    criaDialogFinal();
+                } else {
+                    count++;
+                    rightanswer = generateAll();
+                }
+            }
+        });
+        if (checkAnswerSecond()) {
+            builder.setTitle("Resultado Correto");
+            builder.setMessage("O resultado está correto!");
+            alert = builder.create();
+            alert.show();
+            success++;
+            //new AlertDialog.Builder(this).setTitle("Resultado Correto").setMessage("O resultado está correto!");
+        } else {
+            builder.setTitle("Resultado Errado");
+            builder.setMessage("O resultado correto é: " + String.valueOf(rightanswer));
+            alert = builder.create();
+            alert.show();
+        }
+    }
+
+    public void thirdClick(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alert;
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (count >= 4) {
+                    criaDialogFinal();
+                } else {
+                    count++;
+                    rightanswer = generateAll();
+                }
+            }
+        });
+        if (checkAnswerThird()) {
+            builder.setTitle("Resultado Correto");
+            builder.setMessage("O resultado está correto!");
+            alert = builder.create();
+            alert.show();
+            success++;
+            //new AlertDialog.Builder(this).setTitle("Resultado Correto").setMessage("O resultado está correto!");
+        } else {
+            builder.setTitle("Resultado Errado");
+            builder.setMessage("O resultado correto é: " + String.valueOf(rightanswer));
+            alert = builder.create();
+            alert.show();
+        }
+    }
+
+    private void criaDialogFinal() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("final de jogo!");
+        int nota = success * 20;
+        builder.setMessage("sua nota é de: " + String.valueOf(nota) + " pontos!");
+        builder.setPositiveButton("voltar ao menu", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
 }
